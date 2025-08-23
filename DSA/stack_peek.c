c6cce35b9261020d82cb633dc5424aad749e70ee
KamranRizvi265 <kamranrizvicadet@gmail.com>
2025-08-23 13:24:11 +0530

    commit 13

--- DSA/stack_peek.c
+++ DSA/stack_peek.c
@@ -32,19 +32,6 @@ int isFull(Stack *ptr)
     return 0;
 }
 
-void traversal(Stack *ptr)
-{
-    if(isEmpty(ptr))
-    {
-        return;
-    }
-
-    for(int i=0; i<=ptr->top; i++)
-    {
-        printf("Element: %d\n",ptr->arr[i]);
-    }
-}
-
 void push(Stack *ptr, int value)
 {
     if(isFull(ptr))
@@ -68,6 +55,23 @@ void pop(Stack *ptr)
     ptr->top--;
 }
 
+void peek(Stack *ptr, int pos)
+{
+    if(isEmpty(ptr))
+    {
+        printf("Stack is empty...\n");
+        return;
+    }
+
+    int arrayInd = (ptr->top) - pos + 1;
+    if(arrayInd < 0)
+    {
+        printf("Invalid Position...\n");
+        return;
+    }
+    printf("Value at position %d is %d\n", pos, ptr->arr[arrayInd] );
+}
+
 int main()
 {
     Stack *s = (Stack *) malloc(sizeof(Stack));
@@ -75,39 +79,30 @@ int main()
     s->top = -1;
     s->arr = (int *) malloc(s->size * sizeof(int));
 
-    traversal(s);
-
     push(s, 10);
     push(s, 20);
     push(s, 30);
     push(s, 40);
     push(s, 50);
-
-    traversal(s);
-
     push(s, 60);
     push(s, 70);
     push(s, 80);
     push(s, 90);
     push(s, 100);
-    push(s, 110);
+    push(s, 110); // Stack Overflow
 
-    traversal(s);
-
-    pop(s);
-    pop(s);
-    pop(s);
-    pop(s);
-    pop(s);
-
-    traversal(s);
+    for(int j = 1; j <= s->top+1; j++)
+    {
+        peek(s, j);
+    }
 
     pop(s);
     pop(s);
-    pop(s);
-    pop(s);
-    pop(s);
-    pop(s);
+
+    for(int j = 1; j <= s->top+1; j++)
+    {
+        peek(s, j);
+    }
 
     return 0;
 }
